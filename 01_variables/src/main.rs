e7fdc012d35a58973bbba40f67e15b24e9118548
Cesar <cesararroyocardenas@gmail.com>
2025-04-21 15:22:30 +0000

    examples: variables, inmutability and conditionals

--- 01_variables/src/main.rs
+++ 01_variables/src/main.rs
@@ -1,3 +1,16 @@
+fn main1() {
+    let message = "Name: Cesar, Weight: ";
+    let weight = 155.0;
+
+    let kilos = weight / 2.2;
+    println!("{}{}", message, weight);
+    println!("{}{}", message, kilos);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut message = String::from("Name: Cesar, Weight: ");
+    message.clear();
+    let mut height = 171;
+    height = 182;
+    println!("{}{}", message, height);
 }
